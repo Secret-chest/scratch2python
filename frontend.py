20de14416f9b153f9097d13081c5ea5d669031b6
Secret-chest <vlad.muntoiu@gmail.com>
2021-08-12 19:47:16 +0300

    frontend changes

--- frontend.py
+++ frontend.py
@@ -4,87 +4,79 @@ from PySide6 import QtWidgets
 # from PyQt5 import QtWidgets
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import *
+import requests
 
-
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        outerLayout = QVBoxLayout()
-        layout = QGridLayout()
-        main = QWidget(self)
-        self.setLayout(outerLayout)
-        main.setLayout(layout)
-        main.setObjectName("main")
-        self.setCentralWidget(main)
-        toolbar = QWidget(self)
-        toolbar.setObjectName("toolbar")
-        outerLayout.addWidget(toolbar)
-        outerLayout.addWidget(main)
-        toolbar.setMaximumHeight(64)
-
-        #
-        # TODO
-        #
-
-        self.statusBar().hide()
-        self.show()
-
-
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
-window.setObjectName("window")
-
-
-# Material Design colors
-primaryColor = "#1675d1"
-secondaryColor = "#ccdb37"
-onPrimary = "#ffffff"
-onSecondary = "#000000"
-errorColor = "#d50000"
-onError = "#ffffff"
-background = "#ffffff"
-surface = "#ffffff"
-onBackground = "#000000"
-onSurface = "#000000"
-
-
-style = """
-
-* {
-    font-family: Roboto, apple-system, "Segoe UI", Arial, sans-serif;
-}
-#main, #window {
-    background: #FFFFFF;
-    font-size: 16px;
-    border: none;
-}
-#toolbar {
-    background: #1675d1;
-}
-#toolbar QLabel {
-    color: #FFFFFF;
-    padding: 18px;
-}
-QWidget {
-    border-width: 3px;
-    border-style: solid;
-    border-image: 
-        linear-gradient(
-            to top, 
-            black, 
-            white
-        ) 1 100%;
-}
+app = QApplication()
+window = QWidget()
+layout = QVBoxLayout(window)
+menuBar = QMenuBar()
+layout.addWidget(menuBar)
+toolsMenu = menuBar.addMenu("&Tools")
+accountMenu = menuBar.addMenu("&Scratch")
+helpMenu = menuBar.addMenu("&Help")
+toolsMenu.addAction("&Download JSON")
+toolsMenu.addAction("&Preferences")
+toolsMenu.addAction("View page on &Scratch")
+helpMenu.addAction("&Basic help")
+helpMenu.addAction("Advanced &topics")
+helpMenu.addAction("&About Scratch2Python")
+accountMenu.addAction("&Log in")  # swap with to sign out if logged in
+accountMenu.addAction("Your &profile")
+window.setWindowTitle("Scratch Homepage - Scratch2Python")
 
 
 
-"""
+# TODO
+# Add sources list
+# Add filters
+layout.setContentsMargins(0, 0, 0, 0)
 
-app.setStyleSheet(style)
+app.setStyleSheet(
+    """
 
+    QMainWindow {
+        background: #ffffff;
+    }
+    * {
+        font-family: "Roboto", "Segoe UI", "San Francisco", "DejaVu Sans", "Verdana", "Arial", sans-serif;
+        color: #212121;
+    }
+    QMenuBar::item {
+        padding: 4px;
+    }
+    QMenuBar::item::selected {
+        background-color: #26C6DA;
+    }
+    QMenuBar {
+        background: #ffffff;
+        border: none;
+    }
+    QMenu {
+        padding: 4px;
+        border: none;
+        background: #ffffff;
+    }
+    QMenu::item {
+        padding: 4px;
+    }
+    QMenu::item::selected {
+        background-color: #26C6DA;
+    }
+    QPushButton {
+        background-color: #26C6DA;
+        border: none;
+        border-radius: 0;
+        padding: 8px;
+        outline: none;
+    }
+    QPushButton::pressed {
+        background: #0097A7;
+        color: #ffffff;
+    }
+    """
+)
 
-def placeholder():
-    print("Placeholder")
-
-
-app.exec()
+if __name__ == "__main__":
+    window.setLayout(layout)
+    window.show()
+    app.exec()
