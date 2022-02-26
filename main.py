a47125c0d6f10382afde80de3b1aeeac8641e76e
vlad <vlad.muntoiu@gmail.com>
2022-02-26 07:39:49 +0200

    Increment version number

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = "M13 (development version)"
+__version__ = "M15 (development version)"
 __author__ = "Secret-chest"
 
 import platform
