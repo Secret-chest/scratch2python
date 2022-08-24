65bfe47e5e96a2b1da55d478ae7e1af5f531392c
vlad <vlad.muntoiu@gmail.com>
2022-08-24 19:36:51 +0300

    Bump to 0.0.0

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = "M20 (development version)"
+__version__ = "v0.0.0"
 __author__ = "Secret-chest"
 
 import tkinter.simpledialog
