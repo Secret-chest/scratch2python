7fd689895bbffedb0c00ff6cf643f6bf9681d2a1
vlad <vlad.muntoiu@gmail.com>
2023-04-26 19:17:30 +0300

    Update

--- targetSprite.py
+++ targetSprite.py
@@ -103,9 +103,10 @@ class TargetSprite(pygame.sprite.Sprite):
         self.direction = rot
         print(_("debug-prefix"), _("new-sprite-rotation", rot=rot, name=self.name), file=sys.stderr)
         center = self.sprite.get_rect().center   # TODO get correct rotation centre of sprite
-        print(center, (self.rect.x + center[0], self.rect.y + center[1]), (self.rect.x, self.rect.y), (self.x, self.y), self.direction)
+        print(center, (self.rect.x + center[0], self.rect.y - center[1]), (self.rect.x, self.rect.y), (self.x, self.y), self.direction)
         self.image = pygame.transform.rotate(self.sprite, 90 - self.direction)
-        self.rect = self.image.get_rect(center=(self.rect.x + center[0], self.rect.y + center[1]))
+        self.rect = self.image.get_rect(center=(self.rect.x + center[0], self.rect.y - center[1]))
+        # TODO calculate correct rotation centre
         self.setXy(self.x, self.y)
 
     # Relatively set self rotation (turn)
